0bfa2670f50264537994ba3cb5c61655484b581b
Harun Kusnaedi <harunkusnaedi@gmail.com>
2023-11-22 14:12:39 +0700

    Pengaduan masyarakat react + vite with tailwindcss

---
+++ Pengaduan-masyarakat/Backend/models/MasyarakatModel.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require("sequelize");
+const db = require("../config/Database.js");
+
+const {DataTypes} = Sequelize;
+
+const Masyarakat = db.define("masyarakat",{
+    nik:{
+        type:DataTypes.STRING,
+        primaryKey:true,
+    },
+    nama:DataTypes.STRING,
+    username:DataTypes.STRING,
+    password:DataTypes.STRING,
+    telp:DataTypes.STRING,
+},{freezeTableName:true})
+
+module.exports = Masyarakat;
+
+// (async () => {
+//     await db.sync();
+// })();
